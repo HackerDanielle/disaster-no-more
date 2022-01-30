f6e0359a7d8870d19f3e0cff67a113bc6d2362c8
daniellemoreno525 <daniellemoreno525@tamu.edu>
2022-01-30 09:30:24 -0600

    Using sockets to send info

--- hackathon2022/app.js
+++ hackathon2022/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var axios = require('axios');
+// const socketio = require('socket.io');
+// const http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,4 +21,48 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// const server = http.createServer(app);
+// const io = socketio(server);
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+
+console.log("Beginning socket");
+io.on('connection', socket => { 
+    var config = {
+        method: 'get',
+        url: 'http://api.thedamagecontrol.com/measurements/',
+        headers: { }
+      };
+      
+      axios(config)
+      .then(function (response) {
+        //console.log(JSON.stringify(response.data));
+        var objArray = JSON.stringify(response.data);
+        var jsonData = JSON.parse(objArray);
+        var latestObj = jsonData[jsonData.length - 1];
+      
+        console.log(latestObj);
+        console.log(latestObj.temp);
+      
+        // document.getElementById("status").innerHTML = "Safe";
+        // document.getElementById("temp").innerHTML = latestObj.temp;
+        // document.getElementById("humidity").innerHTML = latestObj.humidity;
+        // document.getElementById("water").innerHTML = latestObj.waterLevel;
+        
+        socket.on('getTemp',() => {
+            socket.emit('temp',latestObj.temp);
+        })
+      
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+});
+
+httpServer.listen(3000);
 module.exports = app;
--- hackathon2022/public/javascripts/dashboard.js
+++ hackathon2022/public/javascripts/dashboard.js
@@ -1,5 +1,7 @@
 /* globals Chart:false, feather:false */
 
+const socket = io();
+
 function openDevice(evt, eventNumber) {
   var i, tabcontent, tablinks;
 
@@ -19,55 +21,90 @@ function openDevice(evt, eventNumber) {
 
 document.getElementById("defaultOpen").click();
 
-(function () {
-  'use strict'
+// (function () {
+//   'use strict'
 
-  feather.replace({ 'aria-hidden': 'true' })
+//   feather.replace({ 'aria-hidden': 'true' })
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-})()
+//   // Graphs
+//   var ctx = document.getElementById('myChart')
+//   // eslint-disable-next-line no-unused-vars
+//   var myChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//       labels: [
+//         'Sunday',
+//         'Monday',
+//         'Tuesday',
+//         'Wednesday',
+//         'Thursday',
+//         'Friday',
+//         'Saturday'
+//       ],
+//       datasets: [{
+//         data: [
+//           15339,
+//           21345,
+//           18483,
+//           24003,
+//           23489,
+//           24092,
+//           12034
+//         ],
+//         lineTension: 0,
+//         backgroundColor: 'transparent',
+//         borderColor: '#007bff',
+//         borderWidth: 4,
+//         pointBackgroundColor: '#007bff'
+//       }]
+//     },
+//     options: {
+//       scales: {
+//         yAxes: [{
+//           ticks: {
+//             beginAtZero: false
+//           }
+//         }]
+//       },
+//       legend: {
+//         display: false
+//       }
+//     }
+//   })
+// })()
+
+socket.emit('temp',latestObj.temp);
+
+socket.on('temp',(tempVal) => {
+  console.log(tempVal);
+  document.getElementById("temp").innerHTML = latestObj.tempVal;
+})
+
+// var axios = require('axios');
+// var config = {
+//   method: 'get',
+//   url: 'http://api.thedamagecontrol.com/measurements/',
+//   headers: { }
+// };
+
+// axios(config)
+// .then(function (response) {
+//   //console.log(JSON.stringify(response.data));
+//   var objArray = JSON.stringify(response.data);
+//   var jsonData = JSON.parse(objArray);
+//   var latestObj = jsonData[jsonData.length - 1];
+
+//   console.log(latestObj);
+//   console.log(latestObj.temp);
+
+//   document.getElementById("status").innerHTML = "Safe";
+//   document.getElementById("temp").innerHTML = latestObj.temp;
+//   document.getElementById("humidity").innerHTML = latestObj.humidity;
+//   document.getElementById("water").innerHTML = latestObj.waterLevel;
+  
+
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
 
